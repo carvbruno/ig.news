eb4a9b25a43060e86c805f2e16df2bcf20fea237
Bruno Carvalho da Silva <bruno.silva@elotech.com.br>
2021-04-02 14:00:05 -0300

    github login

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,23 @@
-import styles from './styles.module.scss'
-
+import { signIn, useSession, signOut } from 'next-auth/client'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
-export function SignInButton() {
-  const isUserLogged = true
+import styles from './styles.module.scss'
 
-  return isUserLogged ? (
+export function SignInButton() {
+  const [session] = useSession()
+
+  return session ? (
     <button className={styles.signInButton}>
       <FaGithub color="#04d361"/>
-      Bruno Carvalho
-      <FiX color="#737388" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX color="#737388" 
+        className={styles.closeIcon} 
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417"/>
       Sign in with Github
     </button>
